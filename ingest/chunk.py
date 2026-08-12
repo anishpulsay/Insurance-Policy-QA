98f8dfee5e9c06d8dd6f1d03b8cbaa3544458b3a
Anish Pulsay <144531069+anishpulsay@users.noreply.github.com>
2026-08-12 18:36:22 +0530

    finished the ingestion

--- ingest/chunk.py
+++ ingest/chunk.py
@@ -4,7 +4,11 @@ from dotenv import load_dotenv
 from pathlib import Path
 import gradio as gr
 from openai import OpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 load_dotenv(override=True)
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
@@ -15,9 +19,27 @@ filenames = glob.glob("data/knowledge-base/*.md")
 for filename in filenames:
     with open(filename, "r",encoding ="utf-8") as file:
         knowledge[filename] = file.read()
-for filename in filenames:
-    doc_type = os.path.basename(filename) 
-    loader = DirectoryLoader(filename glob="**/*.md", loader_cls=TextLoader, loader_kwargs={'encoding': 'utf-8'})
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+
+loader = DirectoryLoader(
+    "data/knowledge-base",
+    glob="*.md",
+    loader_cls=TextLoader,
+    loader_kwargs={"encoding": "utf-8"}
+)
+
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = text_splitter.split_documents(documents)
+#embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+db_name = "vector_db"
+if os.path.exists(db_name):
+    Chroma(persist_directory=db_name, embedding_function=embeddings).delete_collection()
+
+vectorstore = Chroma.from_documents(documents=chunks, embedding=embeddings, persist_directory=db_name)
+print(f"Vectorstore created with {vectorstore._collection.count()} documents")
+
 
 
 
